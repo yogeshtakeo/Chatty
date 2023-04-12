9605b8e8217848ee390cfa3296c28717ab71ca07
yogeshtakeo <yogesh@takeo.ai>
2023-04-11 23:26:16 -0400

    Next Intiation

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World By Chat</h1>");
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World By Chat</h1>");
+app.get("/hello", (req, res) => {
+  res.send("<h1>Hello World By Hello</h1>");
 });
 
 const Port: number = 8080;
