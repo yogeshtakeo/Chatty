2ea93532db7b7d481276b63e927d28260a8da99d
yogeshtakeo <yogesh@takeo.ai>
2023-04-18 12:24:56 -0400

    Tasks 4 created

--- services/jwtservices.ts
+++ services/jwtservices.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const generateJWT = (data): String => {
+export const generateJWT = (data: any): String => {
   console.log(data);
   const token = jwt.sign(data, "191919", { expiresIn: "1d" });
   return token;
